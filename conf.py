7d4024698ffa466d5f52bb45353c337784614f69
zack <919353569@qq.com>
2023-09-15 19:08:05 +0800

    init

---
+++ conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'docs'
+copyright = '2022, zack'
+author = 'zack'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser','sphinx.ext.githubpages','sphinx.ext.todo'
+]
+
+todo_include_todos = True
+
+suppress_warnings = ["myst.header"]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+highlight_langeuage="c,cpp,python,shell,xml,yaml,json,protobuf"
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'zh_CN'
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+# html_static_path = ['_static']
+# html_search_fields = ['title', 'subtitle', 'content','keywords','tags']
+
